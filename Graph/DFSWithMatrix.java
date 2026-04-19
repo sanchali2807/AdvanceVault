f304d35c486818c7e1953c51a2234e198f8f0eec
Sanchali <sanchali280705@gmail.com>
2026-04-19 11:37:23 +0530

    Create DFSWithMatrix.java

---
+++ Graph/DFSWithMatrix.java
@@ -0,0 +1,116 @@
+import java.util.*;
+
+public class Main {
+
+    // Graph container
+    static class Graph {
+        int vertices;
+        List<List<Integer>> adj;
+
+        Graph(int vertices, List<List<Integer>> adj) {
+            this.vertices = vertices;
+            this.adj = adj;
+        }
+    }
+
+    // Build adjacency list
+    static Graph buildGraph(int v, int[][] edges) {
+
+        List<List<Integer>> adj = new ArrayList<>();
+
+        for (int i = 0; i < v; i++) {
+            adj.add(new ArrayList<>());
+        }
+
+        for (int[] e : edges) {
+            int u = e[0];
+            int w = e[1];
+
+            adj.get(u).add(w);
+            adj.get(w).add(u); // remove if directed
+        }
+
+        return new Graph(v, adj);
+    }
+
+    // Convert to adjacency matrix
+    static int[][] buildMatrix(Graph g) {
+
+        int v = g.vertices;
+        int[][] matrix = new int[v][v];
+
+        for (int i = 0; i < v; i++) {
+            for (int neighbor : g.adj.get(i)) {
+                matrix[i][neighbor] = 1;
+            }
+        }
+
+        return matrix;
+    }
+    static void dfs(boolean[]visited,List<List<Integer>> adj,int node){
+        if(visited[node]){
+            return;
+        }
+        visited[node] = true; 
+        // this marks every node 
+        System.out.println(node);
+        for(int nei : adj.get(node)){
+            if(!visited[nei]){
+                // visited[nei] = true;
+                // not here only marks the neighbour
+                dfs(visited,adj,nei);
+            }
+        }
+    }
+    
+    static void dfsMatrix(boolean[] visited, int[][] matrix, int node) {
+
+    visited[node] = true;
+    System.out.print(node + " ");
+
+    for (int i = 0; i < matrix.length; i++) {
+        if (matrix[node][i] == 1 && !visited[i]) {
+            dfsMatrix(visited, matrix, i);
+        }
+    }
+}
+    public static void main(String[] args) {
+
+        int v = 7;
+
+        int[][] inputEdges = {
+            {0,0},{1,2},{1,4},{1,3},
+            {2,5},{2,6},{3,6},{4,5}
+        };
+
+        // build graph
+        Graph g = buildGraph(v, inputEdges);
+
+        // print adjacency list
+        System.out.println("Adjacency List:");
+        for (int i = 0; i < g.vertices; i++) {
+            System.out.println(i + " -> " + g.adj.get(i));
+        }
+
+        // build matrix
+        int[][] matrix = buildMatrix(g);
+
+        // print matrix
+        System.out.println("\nAdjacency Matrix:");
+        for (int i = 0; i < v; i++) {
+            for (int j = 0; j < v; j++) {
+                System.out.print(matrix[i][j] + " ");
+            }
+            System.out.println();
+        }
+        // for weighted we add weight in the inputEdge 2d array
+        
+        boolean visited[] = new boolean[v];
+        for(int i=0;i<v;i++){
+            if(!visited[i]){
+                dfs(visited,g.adj,i);
+            }
+        }
+        // for matrixDFS call we do matrixDFS(visited,matrix,i)
+    }
+}
