bd22a9c4fc0af30548b7d915cd23d5ab2313e14b
Sanchali <sanchali280705@gmail.com>
2026-05-26 17:28:07 +0530

    Create CountTheNumberOfSpecialChar.java

---
+++ Just/CountTheNumberOfSpecialChar.java
@@ -0,0 +1,28 @@
+class Solution {
+    public int numberOfSpecialChars(String word) {
+
+        Set<Character> lower = new HashSet<>();
+        Set<Character> upper = new HashSet<>();
+
+        for(char c : word.toCharArray()){
+
+            if(Character.isLowerCase(c)){
+                lower.add(c);
+            }
+            else{
+                upper.add(c);
+            }
+        }
+
+        int count = 0;
+
+        for(char c : lower){
+
+            if(upper.contains(Character.toUpperCase(c))){
+                count++;
+            }
+        }
+
+        return count;
+    }
+}
